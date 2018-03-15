0cbf26a139003a22adf2d07153f0d5a3385cb39e
EZZAHRAOUI Meriame <meriam.ezzahraoui@gmail.com>
2018-03-15 23:08:31 +0000

    second vesion

--- projet1/Filieres.xaml.cs
+++ projet1/Filieres.xaml.cs
@@ -15,64 +15,37 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace projet1
-{
+{ 
     /// <summary>
     /// Logique d'interaction pour Filieres.xaml
     /// </summary>
     public partial class Filieres : UserControl
     {
-        public static ObservableCollection<Filiere> fl;
-        public static DataClasses1DataContext db;
+        private ListerFiliers listF = new ListerFiliers();
         public Filieres()
         {
             InitializeComponent();
-            db = new DataClasses1DataContext();
-            fl = new ObservableCollection<Filiere>(db.Filiere.ToList());
-        }
-
-        private void radfil_Loaded(object sender, RoutedEventArgs e)
-        {
-            radfil.ItemsSource = fl;
         }
 
         private void ButtonAjouterfil_Click(object sender, RoutedEventArgs e)
         {
             String nom_fil = TextNomFiliere.Text;
             String res = TextResponsableFiliere.Text;
-            var x = (from fil in db.Filiere
-                     where fil.Nom_filiere == nom_fil
-                     select fil).SingleOrDefault();
-            if (x == null)
-            {
-                int id = fl.Count() + 1;
-                Filiere nouveauFiliere = new Filiere();
-                nouveauFiliere.Id_filiere = id;
-                nouveauFiliere.Nom_filiere = nom_fil;
-                nouveauFiliere.responsable = res;
-                db.Filiere.InsertOnSubmit(nouveauFiliere);
-                db.SubmitChanges();
-                fl.Add(nouveauFiliere);
-                MsgErrorFiliere.Text = "Ajouté avec succès";
-            }
-            else
-            {
-                MsgErrorFiliere.Text = "Filière " + nom_fil + " est déjà existe";
-            }
+            MsgErrorFiliere.Text = listF.AjouterFiliere(nom_fil, res);
+
+            TextNomFiliere.Text = "";
+            TextIDFiliere.Text = "";
+            TextResponsableFiliere.Text = "";
         }
 
         private void ButtonModifierFil_Click(object sender, RoutedEventArgs e)
         {
             Filiere f = (Filiere)radfil.SelectedItem;
-            int id_fil = Convert.ToInt32(TextIDFiliere.Text);
-            String res = TextResponsableFiliere.Text;
-            var x = (from fil in db.Filiere
-                     where fil.Id_filiere == id_fil
-                     select fil).SingleOrDefault();
-            x.Id_filiere = Convert.ToInt32(TextIDFiliere.Text);
-            x.Nom_filiere = TextNomFiliere.Text;
-            x.responsable = res;
-            db.SubmitChanges();
-            MsgErrorFiliere.Text = "Modification effectuée avec succès";
+            MsgErrorFiliere.Text = listF.ModifierFiliere(f, TextNomFiliere.Text, TextResponsableFiliere.Text);
+            TextNomFiliere.Text = "";
+            TextIDFiliere.Text = "";
+            TextResponsableFiliere.Text = "";
+
         }
 
         private void buttonSupprimerFil_Click(object sender, RoutedEventArgs e)
@@ -82,13 +55,11 @@ namespace projet1
             MessageBoxResult op = MessageBox.Show("Voulez vous supprimer la filière " + nom_fil + " ?", "confirmation de suppression", MessageBoxButton.YesNo);
             if (op == MessageBoxResult.Yes)
             {
-                fl.Remove((Filiere)radfil.SelectedItem);
-                var x = (from fil in db.Filiere
-                         where fil.Id_filiere == id_fil
-                         select fil).SingleOrDefault();
-                db.Filiere.DeleteOnSubmit(x);
-                db.SubmitChanges();
+                MessageBox.Show(listF.SupprimerFiliere((Filiere)radfil.SelectedItem));
             }
+            TextNomFiliere.Text = "";
+            TextIDFiliere.Text = "";
+            TextResponsableFiliere.Text = "";
         }
     }
 }
--- projet1/ListerFiliers.cs
+++ projet1/ListerFiliers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,185 @@ namespace projet1
 {
     public class ListerFiliers
     {
-        public static ObservableCollection<etudiant> MyItems { get; set; }
-        public static ObservableCollection<Filiere> ListeFilieres { get; set; }
+       // public static ObservableCollection<etudiant> MyItems { get; set; }
+        public static ObservableCollection<OperationFiliere> ListeFilieres { get; set; }
 
         public static DataClasses1DataContext cl;
 
-        static ListerFiliers()
+        public ListerFiliers()
         {
             cl = new DataClasses1DataContext();
-            MyItems = new ObservableCollection<etudiant>(cl.etudiant.ToList());
-            ListeFilieres = new ObservableCollection<Filiere>(cl.Filiere.ToList());
+           // MyItems = new ObservableCollection<etudiant>(cl.etudiant.ToList());
+            ListeFilieres = new ObservableCollection<OperationFiliere>(getAllFiliere());
         }
+
+        public List<OperationFiliere> getAllFiliere() {
+            List<OperationFiliere> l = new List<OperationFiliere>();
+            var x = from fil in cl.Filiere
+                    select fil;
+
+            foreach(var i in x)
+            {
+                OperationFiliere f = new OperationFiliere();
+                f.Id = i.Id_filiere;
+                f.NomFiliere = i.Nom_filiere;
+                f.Responsable = i.responsable;
+                l.Add(f);
+            }
+
+            return l;
+        }
+
+        public String AjouterFiliere(String nomFil, String resp)
+        {
+            try
+            {
+                var x = (from fil in cl.Filiere
+                         where fil.Nom_filiere == nomFil
+                         select fil).SingleOrDefault();
+                // si dans la base de donnée Nom filiere n'est pas définie unique
+                if (x == null)
+                {
+                    Filiere nouveauFiliere = new Filiere();
+                    nouveauFiliere.Nom_filiere = nomFil;
+                    nouveauFiliere.responsable = resp;
+                    cl.Filiere.InsertOnSubmit(nouveauFiliere);
+                    cl.SubmitChanges();
+                    var y = (from fil in cl.Filiere
+                             where fil.Nom_filiere == nomFil
+                             select fil.Id_filiere).SingleOrDefault();
+                    OperationFiliere f = new OperationFiliere();
+                    f.Id= (int)y;
+                    f.NomFiliere = nouveauFiliere.Nom_filiere;
+                    f.Responsable = nouveauFiliere.responsable;
+                    ListeFilieres.Add(f);
+                    return "Ajouté avec succès";
+                }
+                else
+                    return "Filière " + nomFil + " existe deja";
+
+
+            }
+            catch
+            {
+                return "Filière " + nomFil + " existe deja";
+            }
+        }
+
+        public String ModifierFiliere(Filiere f, String nomFil, String resp)
+        {
+            try
+            {
+
+                int id_fil = f.Id_filiere;
+                var x = (from fil in cl.Filiere
+                         where fil.Id_filiere == id_fil
+                         select fil).SingleOrDefault();
+
+                var y = (from fil in cl.Filiere
+                         where fil.Nom_filiere == nomFil
+                         select new { nom = fil.Nom_filiere, id = fil.Id_filiere }).SingleOrDefault();
+                // si dans la base de donnée Nom filiere n'est pas définie unique
+                if (y != null && y.id!=id_fil )
+                {
+                    return "Filière " + nomFil + " existe deja";
+                }
+                else
+                {
+                    x.Nom_filiere = nomFil;
+                    x.responsable = resp;
+                    cl.SubmitChanges();
+                    return "Modification effectuée avec succès";
+                }
+                    
+                
+            }
+            catch
+            {
+                return "Une erreur est survenu lors d'enregistremen des changements ";
+            }
+
+        }
+
+        public String SupprimerFiliere(Filiere f)
+        {
+            try
+            {
+                OperationFiliere fil = new OperationFiliere();
+                fil.Id = f.Id_filiere;
+                fil.NomFiliere = f.Nom_filiere;
+                fil.Responsable = f.responsable;
+                ListeFilieres.Remove(fil);
+                var x = (from filier in cl.Filiere
+                         where filier.Id_filiere == f.Id_filiere
+                         select filier).SingleOrDefault();
+                cl.Filiere.DeleteOnSubmit(x);
+                cl.SubmitChanges();
+                return "La filiere " + f.Nom_filiere + " est supprimée de la base de donnée";
+            }
+            catch
+            {
+                return "Une erreur est survenu lors de la suppression de la filiere Veiller ressayer plus tard";
+            }
+        }
+
+
+    }
+
+    public class OperationFiliere : INotifyPropertyChanged
+    {
+        private int id;
+        private String nomFiliere;
+        private String responsable;
+
+
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                id = value;
+                NotifyPropertyChanged("Id");
+            }
+        }
+        public string NomFiliere {
+            get
+            {
+                return this.nomFiliere;
+            }
+            set
+            {
+                nomFiliere = value;
+                NotifyPropertyChanged("NomFiliere");
+            }
+        }
+        public string Responsable {
+            get
+            {
+                return this.responsable;
+            }
+            set
+            {
+                responsable = value;
+                NotifyPropertyChanged("Responsable");
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler Handler = PropertyChanged;
+            if (Handler != null)
+            {
+                Handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
